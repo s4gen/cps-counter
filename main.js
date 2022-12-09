da93652a8d5d615aeb3401223746e5dd547f5fc6
s4gen <87096163+s4gen@users.noreply.github.com>
2022-12-09 13:03:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const button = document.getElementById('button')
 const display = document.getElementById('display')
 let clicks = 0
+display.innerHTML = clicks
 button.addEventListener('click', () => {
   clicks++
   display.innerHTML = clicks
